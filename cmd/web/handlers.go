19c03eb40ca60bb7171dd30132c7ddb310bb07fb
GergesHany <gergeshany179@gmail.com>
2024-12-16 20:37:13 +0200

    add Panic recovery & Advanced routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,13 +8,11 @@ import (
 
 	"snippetbox.alexedwards.net/internal/models"
 
+	"github.com/julienschmidt/httprouter"
+
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -29,11 +27,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		app.notFound(w)
-		return
-	}
+	
+   params := httprouter.ParamsFromContext(r.Context());
+	
+   id, err := strconv.Atoi(params.ByName("id"))
+   if err != nil || id < 1 {
+	  app.notFound(w)
+	  return
+   }
 
    snippets, err := app.snippets.Get(id)
    if err != nil {
@@ -51,13 +52,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
    app.render(w, http.StatusOK, "view.tmpl.html", data)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Display the form for creating a new snippet..."))
+}
 
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request){
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n-Kobayashi Issa"
 	expires := 7
