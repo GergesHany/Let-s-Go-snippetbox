230b2073743aef0420fb7ee577b6aceef1dc736f
GergesHany <gergeshany179@gmail.com>
2024-12-22 21:48:30 +0200

    (Parsing & validate) data form & Displaying errors and repopulating fields

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,12 +5,21 @@ import (
 	"fmt"
 	"net/http"
 	"strconv" // for converting string to int
+	"strings"
+	"unicode/utf8"
 
 	"snippetbox.alexedwards.net/internal/models"
 
 	"github.com/julienschmidt/httprouter"
 
 )
+
+type snippetCreateForm struct {
+	Title string 
+	Content string
+	Expires int
+	FieldErrors map[string]string
+}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request){
 
@@ -20,7 +29,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	data := app.newTemplateData(r)
+	data := app.newTemplateData(r) 
 	data.Snippets = snippets
 
 	app.render(w, http.StatusOK, "home.tmpl.html", data)
@@ -29,7 +38,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	
    params := httprouter.ParamsFromContext(r.Context());
-	
+
    id, err := strconv.Atoi(params.ByName("id"))
    if err != nil || id < 1 {
 	  app.notFound(w)
@@ -39,7 +48,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
    snippets, err := app.snippets.Get(id)
    if err != nil {
 	   if errors.Is(err, models.ErrNoRecord) {
-		   app.notFound(w)
+   		   app.notFound(w)
 	   } else {
 		   app.serverError(w, err)
 	   }
@@ -53,21 +62,71 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Display the form for creating a new snippet..."))
+   data := app.newTemplateData(r)
+   
+   data.Form = snippetCreateForm{
+	 Expires: 365,
+   }
+
+   app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request){
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n-Kobayashi Issa"
-	expires := 7
 
-	// Insert the snippet into the database
+    err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form := snippetCreateForm{
+		Title: title,
+		Content: content,
+		Expires: expires,
+		FieldErrors: make(map[string]string),
+	}
+
+	// Validate the title field.
+	if strings.TrimSpace(title) == "" {
+		form.FieldErrors["title"] = "Title cannot be blank"
+	}else if utf8.RuneCountInString(title) > 100 {
+		form.FieldErrors["title"] = "Title cannot be longer than 100 characters"
+	}
+
+    // Validate the content field.
+	if strings.TrimSpace(content) == "" {
+		form.FieldErrors["content"] = "Content cannot be blank"
+	}
+
+	// validate the expires field.
+	if expires != 1 && expires != 7 && expires != 365 {
+		form.FieldErrors["expires"] = "Please select a valid expiration time"
+	}
+
+	// If there are any errors, redisplay the form.
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		// StatusUnprocessableEntity: 422
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
+		return
+	}
+
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
-	   app.serverError(w, err)
-	   return
+		app.serverError(w, err)
+		return
 	}
 
 	// Redirect the user to the relevant page for the snippet.
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -9,9 +9,10 @@ import (
 )
 
 type templateData struct {
-   CurrentYear int
-   Snippet *models.Snippet
-   Snippets []*models.Snippet
+   CurrentYear  int
+   Snippet      *models.Snippet
+   Snippets     []*models.Snippet
+   Form         any
 }
 
 func humanDate(t time.Time) string {
